344156976f194791fe85474418a2d4e3d858b95d
Brandon Sepúlveda Martínez <81381414+BrandonSepu@users.noreply.github.com>
2023-07-18 01:08:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get('/', function(req, res) {
 
 // Otros endpoints o configuraciones de tu servidor
 const host = 'nodejs-production-5c29.up.railway.app';
-const port = process.env.PORT || 4200;
+const port = process.env.PORT || 3000;
 app.listen(port, host, function() {
   console.log(`Aplicación Node.js desplegada en ${host}:${port}`);
 });
