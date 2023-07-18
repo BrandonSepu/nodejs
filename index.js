72bc30fae50d8fd706cb0f7ea879e01c6b2c6e52
Brandon Sepúlveda Martínez <81381414+BrandonSepu@users.noreply.github.com>
2023-07-18 00:59:48 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get('/', function(req, res) {
 });
 
 // Otros endpoints o configuraciones de tu servidor
-
-app.listen(4200, function() {
-  console.log('Aplicación Angular desplegada en el puerto 4200');
+const port = process.env.PORT || 4200;
+app.listen(port, function() {
+  console.log(`Aplicación Node.js desplegada en el puerto ${port}`);
 });
